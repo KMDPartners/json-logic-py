3b590969de8a1013e3e56430506c16711359a99a
hoherchak <hryhorii@creditninja.com>
2020-10-28 15:18:11 +0200

    Add tests

--- tests.py
+++ tests.py
@@ -344,9 +344,11 @@ class JSONLogicTest(unittest.TestCase):
         self.assertTrue(jsonLogic({"in": ["Spring", "Springfield"]}))
         self.assertTrue(jsonLogic({"in": ["Spring", ["Spring", "summer"]]}))
         self.assertTrue(jsonLogic({"in": [{"var": "state"}, ["tx", "al"]]}, data={"state": "tx"}))
+        self.assertFalse(jsonLogic({"in": [{"var": "state"}, ["tx", "al"]]}, data={"state": "ca"}))
         self.assertTrue(jsonLogic({"!in": ["Summer", "Springfield"]}))
         self.assertTrue(jsonLogic({"!in": ["Winter", ["Spring", "summer"]]}))
         self.assertTrue(jsonLogic({"!in": [{"var": "state"}, ["tx", "al"]]}, data={"state": "ca"}))
+        self.assertFalse(jsonLogic({"!in": [{"var": "state"}, ["tx", "al"]]}, data={"state": "tx"}))
 
     def test_cat(self):
         """
